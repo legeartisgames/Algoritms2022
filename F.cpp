6fc7ef64eefe3a75afea5c98604a9294c8cabc33
Legeartis Games <100529767+legeartisgames@users.noreply.github.com>
2022-10-20 16:53:07 +0300

    LSD became optimised
    Sorry for 0XFF but I really don't see nicer way to compute that thing

--- F.cpp
+++ F.cpp
@@ -1,54 +1,57 @@
 #include <iostream>
 #include <vector>
 
-using std::cin;
-using std::cout;
 using std::vector;
-void LSDSort(vector<uint64_t>& shifted, vector<int>& shifted256,
-             vector<int>& order_of_numbers, vector<int>& new_order,
-             int order_of_byte) {
-  int a_size = (int)order_of_numbers.size();
-  vector<int> cnt(256, 0);
-  for (int i = 0; i < a_size; ++i) {
+
+void LSDSortByByte(vector<uint64_t>& shifted, vector<int>& shifted256,
+                   vector<int>& order_of_numbers, vector<int>& new_order,
+                   int order_of_byte) {
+  const size_t kPOW8OF2 = 256;
+  size_t a_size = order_of_numbers.size();
+  vector<int> cnt(kPOW8OF2, 0);
+  for (size_t i = 0; i < a_size; ++i) {
     if (order_of_byte > 0) {
       shifted[i] = shifted[i] >> 8;
     }
     shifted256[i] = shifted[i] & 0xFF;
     ++cnt[shifted256[i]];
   }
-  vector<int> pref(256, 0);
-  for (int i = 1; i < 256; ++i) {
+  vector<int> pref(kPOW8OF2, 0);
+  for (size_t i = 1; i < kPOW8OF2; ++i) {
     pref[i] = pref[i - 1] + cnt[i - 1];
   }
-  for (int i = 0; i < a_size; ++i) {
+  for (size_t i = 0; i < a_size; ++i) {
     new_order[pref[shifted256[order_of_numbers[i]]]++] = order_of_numbers[i];
   }
+  order_of_numbers.swap(new_order);
 }
 
 int main() {
-  int length_of_ar;
-  cin >> length_of_ar;
+  size_t length_of_ar;
+  std::cin >> length_of_ar;
+
   uint64_t* array = new uint64_t[length_of_ar];
+  for (size_t i = 0; i < length_of_ar; ++i) {
+    std::cin >> array[i];
+  }
+
   vector<int> order_of_numbers;
   order_of_numbers.reserve(length_of_ar);
   vector<uint64_t> shifted;
   shifted.reserve(length_of_ar);
   vector<int> shifted256(length_of_ar, 0);
-  for (int i = 0; i < length_of_ar; ++i) {
-    cin >> array[i];
+
+  for (size_t i = 0; i < length_of_ar; ++i) {
     shifted.push_back(array[i]);
     order_of_numbers.push_back(i);
   }
+
   vector<int> new_order(length_of_ar, 0);
   for (int i = 0; i < 7; ++i) {
-    if (i % 2 == 0) {
-      LSDSort(shifted, shifted256, order_of_numbers, new_order, i);
-    } else {
-      LSDSort(shifted, shifted256, new_order, order_of_numbers, i);
-    }
+    LSDSortByByte(shifted, shifted256, order_of_numbers, new_order, i);
   }
-  for (int i = 0; i < length_of_ar; ++i) {
-    cout << array[new_order[i]] << '\n';
+  for (size_t i = 0; i < length_of_ar; ++i) {
+    std::cout << array[order_of_numbers[i]] << '\n';
   }
   delete[] array;
   return 0;

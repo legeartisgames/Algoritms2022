681cec9afe08f911df11002a99c32b0ff3bb2ace
Legeartis Games <100529767+legeartisgames@users.noreply.github.com>
2022-10-27 16:39:43 +0300

    Obscure pieces of code replaced
    Вот как я могу сделать более красивым разбиение main? Ускоренный ввод/вывод убрать из него нельзя, выделять всё остальное в отдельную функцию бессмысленно, делать отдельную функцию для ввода или вывода тоже глупо. Вот и приходим к текущему виду main. В конце концов, main и есть связующий центр задачи, раз код про это!
    ID - 	73165995

--- G.cpp
+++ G.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include <vector>
-using std::cin;
-using std::cout;
-using std::endl;
-using std::vector;
 
 void SwapElements(int* array, int ind1, int ind2) {
-  int c = array[ind1];
+  int temp = array[ind1];
   array[ind1] = array[ind2];
-  array[ind2] = c;
+  array[ind2] = temp;
 }
 int Partition(int* array, int start_ind, int end_ind, int pivot_ind) {
   int pivot_value = array[pivot_ind];
@@ -31,67 +27,74 @@ int Partition(int* array, int start_ind, int end_ind, int pivot_ind) {
     --right;
   }
 }
-int Median(int* array, int s, int e, int m) {
-  if (s + 1 == e) {
-    if (array[e] > array[s]) {
-      SwapElements(array, e, s);
+int Median(int* array, size_t left, size_t right, size_t med) {
+  if (left + 1 == right) {
+    if (array[right] > array[left]) {
+      SwapElements(array, right, left);
     }
-    return e;
+    return right;
   }
-  if (array[s] > array[m]) {
-    if (array[e] > array[s]) {
-      SwapElements(array, m, s);
-      return m;
+  if (array[left] > array[med]) {
+    if (array[right] > array[left]) {
+      SwapElements(array, med, left);
+      return med;
     }
-    if (array[m] >= array[e]) {
-      SwapElements(array, s, e);
-      return m;
+    if (array[med] >= array[right]) {
+      SwapElements(array, left, right);
+      return med;
     }
-    SwapElements(array, s, e);
-    SwapElements(array, s, m);
-    return m;
+    SwapElements(array, left, right);
+    SwapElements(array, left, med);
+    return med;
   }
-  if (array[e] >= array[m]) {
-    return m;
+  if (array[right] >= array[med]) {
+    return med;
   }
-  if (array[e] >= array[s]) {
-    SwapElements(array, e, m);
-    return m;
+  if (array[right] >= array[left]) {
+    SwapElements(array, right, med);
+    return med;
   }
-  SwapElements(array, m, e);
-  SwapElements(array, s, m);
-  return m;
+  SwapElements(array, med, right);
+  SwapElements(array, left, med);
+  return med;
 }
 
 void QuickSort(int* array, int start_ind, int end_ind) {
   if (start_ind < end_ind) {
-    int s =
-        start_ind + (end_ind - start_ind) / 20 *
-                        abs((end_ind - start_ind + array[start_ind] / 10) % 10);
-    int e = start_ind + 1 +
-            (end_ind - 1 - start_ind) / 10 *
-                abs((end_ind - start_ind + array[start_ind] / 10) % 10);
-    int m = s + (e - s) / 2;
-    int pivot_ind = Median(array, s, e, m);
+    const int kDivide = 10;  // suitable to randoming pivot
+    int left =
+        start_ind +
+        (end_ind - start_ind) / (2 * kDivide) *
+            abs((end_ind - start_ind + array[start_ind] / kDivide) % kDivide);
+    int right =
+        start_ind + 1 +
+        (end_ind - 1 - start_ind) / kDivide *
+            abs((end_ind - start_ind + array[start_ind] / kDivide) % kDivide);
+    int median = left + (right - left) / 2;
+    int pivot_ind = Median(array, left, median, right);
     int new_pivot_ind = Partition(array, start_ind, end_ind, pivot_ind);
     QuickSort(array, start_ind, new_pivot_ind);
     QuickSort(array, new_pivot_ind + 1, end_ind);
   }
 }
+
 int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
-  int length_of_array;
-  cin >> length_of_array;
+
+  size_t length_of_array;
+  std::cin >> length_of_array;
   int* array = new int[length_of_array];
-  for (int i = 0; i < length_of_array; ++i) {
-    cin >> array[i];
+  for (size_t i = 0; i < length_of_array; ++i) {
+    std::cin >> array[i];
   }
+
   QuickSort(array, 0, length_of_array - 1);
-  for (int i = 0; i < length_of_array; ++i) {
-    cout << array[i] << ' ';
+  for (size_t i = 0; i < length_of_array; ++i) {
+    std::cout << array[i] << ' ';
   }
   delete[] array;
+
   return 0;
 }
